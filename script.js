5e14a8963fd2fa1dbbd39e4fe6e57c45cdcfaa6e
Joseph Teka <179788482+Josy216@users.noreply.github.com>
2025-03-31 22:08:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const diary = document.querySelector(".diary");
+const btn = document.querySelector(".btn");
+let para = document.querySelectorAll(".para");
+
+function diearyplace() {
+  const storedData = localStorage.getItem("para");
+  if (storedData) {
+    diary.innerHTML = storedData;
+  }
+}
+diearyplace();
+
+function newStorage() {
+  localStorage.setItem("para", diary.innerHTML);
+}
+
+btn.addEventListener("click", () => {
+  let pElement = document.createElement("P");
+  let delimage = document.createElement("img");
+  pElement.className = "para";
+  pElement.setAttribute("contenteditable", "true");
+  delimage.src = "loaading.jpg";
+  diary.appendChild(pElement).appendChild(delimage);
+  newStorage();
+});
+
+diary.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    newStorage();
+  } else if (e.target.tagName === "P") {
+    para = document.querySelectorAll(".para");
+    para.forEach((notes) => {
+      notes.onkeyup = function () {
+        newStorage();
+      };
+    });
+  }
+});
